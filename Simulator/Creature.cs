3ac95002292a075ca87c354b1f8f8738c13d4f42
MariuszLezon <mariuszlezon21@gmail.com>
2024-12-17 21:50:16 +0100

    Stwory

---
+++ Simulator/Creature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; } = "Unknown";
+    public int Level { get; set; } = 1;
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name} at level {Level}!");
+    }
+
+    public string Info => $"{Name} <{Level}>";
+}
